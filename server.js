739ab44ee87be894f4c4f3b76aec524af4331ae3
Ricky27 <Rickyrenteria7@gmail.com>
2022-02-14 21:31:16 -0800

    created 404 err

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const { animals } = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -34,14 +35,28 @@ function filterByQuery(query, animalsArray) {
 }
 
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
-
     }
     res.json(results);
 })
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    // res.json(result);
+});
 
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!');
